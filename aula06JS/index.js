daa69ddef78550b845aea0b733e9b38aa54c2c74
pedrosoaresetec-commits <pedrosoaresetec@gmail.com>
2026-03-16 08:50:35 -0300

    Add files via upload

---
+++ aula06JS/index.js
@@ -0,0 +1,60 @@
+/*exercicio 1
+let numero1 = prompt("digite um numero: ");
+var resultado = numero1 % 2;
+if (resultado == 0){
+    alert("o numero é par");
+
+}else{
+    alert("o numero é impar")
+}
+*/
+
+/* exercicio 2
+let idade = prompt("Qual é o seu idade?");
+if (idade <= 18) {
+    alert("Você é menor de idade");
+} else{
+    alert("Você é maior de idade");
+}
+*/
+
+
+/* exercicio 3
+var i= 1;
+while (i<11){
+    console.log(i);
+    i++;
+}
+*/
+
+/* exercicio 4
+let numero1 = prompt("digite um numero até 10: ");
+while (numero1>0){
+    alert(numero1);
+    numero1 = numero1 - 1;
+}
+alert ("FIM!")
+*/
+
+/* exercicio 5
+var n1 = parseInt(prompt("Digite um número para ver a tabuada (até 10):"));
+
+for (var i = 1; i <= 10; i++) {
+    console.log(n1 + " x " + i + " = " + (n1 * i));
+}
+*/
+
+/* exercicio 6
+var n1 = parseInt(prompt("Digite um número para ver a sua soma (até 20):"));
+
+for (var i = 1; i <= 20; i++) {
+    console.log(n1 + " + " + i + " = " + (n1 + i));
+}
+*/
+
+var n1 = parseInt(prompt("Digite um número para saber se é primo ou não:"));
+if (n1<2) {
+    
+} else {
+    
+}
